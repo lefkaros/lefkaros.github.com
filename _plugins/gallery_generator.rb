87e62962c130bcad537f742c85ad02fe6ae32d5d
Tasos Sagiotis <tsagiotis@gmail.com>
2013-12-20 13:41:07 +0200

    gallery work

--- _plugins/gallery_generator.rb
+++ _plugins/gallery_generator.rb
@@ -17,7 +17,7 @@ module Jekyll
     def initialize(site, base, dir, galleries)
       @site = site
       @base = base
-      @dir = "/#{dir}"
+      @dir = dir.gsub("source/", "") 
       @name = "index.html"
 
       self.process(@name)
@@ -34,17 +34,42 @@ module Jekyll
     end
   end
 
+  class GalleryImage
+    attr_accessor :filename
+    
+    def initialize(filename)
+      @filename = filename
+    end
+    
+    def title
+      File.basename( self.filename, ".*" )    
+    end
+        
+    def comment
+    end
+    
+    def to_liquid
+      {
+       'filename'=>self.filename,
+       'title' => self.title,
+       'comment' => self.comment
+      }
+    end    
+  end
+
   class GalleryPage < Page
     def initialize(site, base, dir, gallery_name)
       @site = site
       @base = base
-      @dir = "/#{dir}"
+      @dest_dir = dir.gsub("source/", "")
+      @dir = @dest_dir
       @name = "index.html"
       @images = []
 
       best_image = nil
       max_size_x = 400
       max_size_y = 400
+      scale_method = site.config["gallery"]["scale_method"] || "fit"
       begin
         max_size_x = site.config["gallery"]["thumbnail_size"]["x"]
       rescue
@@ -57,21 +82,29 @@ module Jekyll
       self.read_yaml(File.join(base, "_layouts"), "gallery_page.html")
       self.data["gallery"] = gallery_name
       gallery_title_prefix = site.config["gallery"]["title_prefix"] || "Photos: "
-      gallery_name = gallery_name.gsub("_", " ").gsub(/\w+/) {|word| word.capitalize}
+      g_name = site.config["gallery"]["galleries"][self.data["gallery"]]["name"]
+      
+      if g_name == nil
+        gallery_name = gallery_name.gsub("_", " ").gsub(/\w+/) {|word| word.capitalize}
+      else
+        gallery_name = g_name
+      end
       self.data["name"] = gallery_name
       self.data["title"] = "#{gallery_title_prefix}#{gallery_name}"
-      thumbs_dir = "#{site.dest}/#{dir}/thumbs"
+      thumbs_dir = "#{site.dest}/#{@dest_dir}/thumbs"
 
       FileUtils.mkdir_p(thumbs_dir, :mode => 0755)
       Dir.foreach(dir) do |image|
         if image.chars.first != "." and image.downcase().end_with?(*$image_extensions)
-          @images.push(image)
+          i = GalleryImage.new(image)
+          
+          @images.push(i)
           best_image = image
-          @site.static_files << GalleryFile.new(site, base, "#{dir}/thumbs/", image)
+          @site.static_files << GalleryFile.new(site, base, "#{@dest_dir}/thumbs/", image)
           if File.file?("#{thumbs_dir}/#{image}") == false or File.mtime("#{dir}/#{image}") > File.mtime("#{thumbs_dir}/#{image}")
             begin
               m_image = ImageList.new("#{dir}/#{image}")
-              m_image.resize_to_fit!(max_size_x, max_size_y)
+              m_image.send("resize_to_#{scale_method}!", max_size_x, max_size_y)
               puts "Writing thumbnail to #{thumbs_dir}/#{image}"
               m_image.write("#{thumbs_dir}/#{image}")
             rescue
@@ -92,6 +125,14 @@ module Jekyll
         self.data["date_time"] = EXIFR::JPEG.new("#{dir}/#{best_image}").date_time.to_i
       rescue
       end
+      
+      if File.file?("#{dir}/description.md")
+        contents = File.read("#{dir}/description.md")
+        converter = Jekyll::Converters::Markdown::RedcarpetParser.new(site.config)
+        d = converter.convert(contents)
+        self.data["description"] = d
+      end
+      
     end
   end
 
